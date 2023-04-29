825a2e72fa404050e6b7f526121eee47a5fa5dca
arfshl <98870352+arfshl@users.noreply.github.com>
2023-04-29 03:32:14 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo "Installing Mullvad Browser..."
-sudo wget https://mullvad.net/en/download/browser/linux64/latest -P /opt
-sudo cd /opt
+cd /opt
+sudo curl -o mullvad-browser.tar.xz https://mullvad.net/en/download/browser/linux64/latest
 sudo tar xvf mullvad-browser-linux64*.tar.xz
-sudo rm mullvad-browser-linux64*.tar.xz
-sudo cd /opt/mullvad-browser-linux64*
+sudo rm mullvad-browser.tar.xz
+sudo cd /opt/mullvad-browser
 sudo chmod +x start-mullvad-browser.desktop
 sudo ./start-mullvad-browser.desktop --register-app
